ebd8114c26dce2aaa194a36c8139f2ef68e4205e
Khin Phyu  Khine <kpkhine01@gmail.com>
2025-01-31 21:39:10 -0800

    initial application and chakra ui set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, HStack} from "@chakra-ui/react";
+
+const App = () => {
+  return (
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+  )
+}
+
+export default App
